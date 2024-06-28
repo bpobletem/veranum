18a4e76722f6d89a5b47eaa331b7d0ec77d36ae9
faavsun <f.aburto@duocuc.cl>
2024-06-27 21:08:39 -0400

    reportes/habitacion/reservas/servicios

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Hotel, TipoServicio, Servicio, TipoHabitacion, Habitacion, Pasajero, HabitacionReserva, Reserva, HistorialPrecios, Administrador, Oferta, OfertaReserva, OfertaServicio
 
+
 class AdmTipoServicio(admin.ModelAdmin):
     list_display = ['nombre']
 
-admin.site.register(TipoServicio, AdmTipoServicio)
+
+admin.site.register(Hotel)
+admin.site.register(TipoServicio)
+admin.site.register(Servicio)
+admin.site.register(TipoHabitacion)
+admin.site.register(Habitacion)
+admin.site.register(Pasajero)
+admin.site.register(HabitacionReserva)
+admin.site.register(Reserva)
+admin.site.register(HistorialPrecios)
+admin.site.register(Administrador)
+admin.site.register(Oferta)
+admin.site.register(OfertaReserva)
+admin.site.register(OfertaServicio)
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import TipoServicio, Servicio
+from .models import TipoServicio, Servicio, Hotel, Habitacion, Servicio, Reserva
 from .forms import TipoServicioForm, ServicioForm
+
+
+def reportes(request):
+    return render(request, 'aplicacion/reportes.html')
+
+
+def reporte_habitaciones(request):
+    habitaciones = Habitacion.objects.all()
+    return render(request, 'aplicacion/reporte_habitaciones.html', {'habitaciones': habitaciones})
+
+
+def reporte_servicios(request):
+    servicios = Servicio.objects.all()
+    return render(request, 'aplicacion/reporte_servicios.html', {'servicios': servicios})
+
+
+def reporte_reservas(request):
+    reservas = Reserva.objects.all()
+    return render(request, 'aplicacion/reporte_reservas.html', {'reservas': reservas})
+
 
 def tipo_servicio_list(request):
     servicios = TipoServicio.objects.all()
     return render(request, 'aplicacion/tipo_servicio_list.html', {'servicios': servicios})
+
 
 def tipo_servicio_create(request):
     if request.method == 'POST':
@@ -15,6 +36,7 @@ def tipo_servicio_create(request):
     else:
         form = TipoServicioForm()
     return render(request, 'aplicacion/tipo_servicio_form.html', {'form': form})
+
 
 def tipo_servicio_update(request, id):
     servicio = get_object_or_404(TipoServicio, id=id)
@@ -27,12 +49,14 @@ def tipo_servicio_update(request, id):
         form = TipoServicioForm(instance=servicio)
     return render(request, 'aplicacion/tipo_servicio_form.html', {'form': form})
 
+
 def tipo_servicio_delete(request, id):
     servicio = get_object_or_404(TipoServicio, id=id)
     if request.method == 'POST':
         servicio.delete()
         return redirect('tipo_servicio_list')
     return render(request, 'aplicacion/tipo_servicio_confirm_delete.html', {'servicio': servicio})
+
 
 def crear_servicio(request):
     if request.method == "POST":
@@ -44,9 +68,11 @@ def crear_servicio(request):
         form = ServicioForm()
     return render(request, 'aplicacion/crear_servicio.html', {'form': form})
 
+
 def lista_servicios(request):
     servicios = Servicio.objects.all()
     return render(request, 'aplicacion/lista_servicios.html', {'servicios': servicios})
+
 
 def editar_servicio(request, id):
     servicio = get_object_or_404(Servicio, id=id)
@@ -59,9 +85,10 @@ def editar_servicio(request, id):
         form = ServicioForm(instance=servicio)
     return render(request, 'aplicacion/editar_servicio.html', {'form': form, 'servicio': servicio})
 
+
 def eliminar_servicio(request, id):
     servicio = get_object_or_404(Servicio, id=id)
     if request.method == "POST":
         servicio.delete()
-        return redirect('lista_servicios') 
+        return redirect('lista_servicios')
     return render(request, 'aplicacion/eliminar_servicio.html', {'servicio': servicio})
